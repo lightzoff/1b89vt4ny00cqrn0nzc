7cfbf6e5b690b880c49b397fb89453a2807d6b21
AaronM1M1 <aaronm888@hotmail.com>
2016-05-05 11:51:45 +1200

    WhatsUponWhatsOn

--- app/src/main/java/basementtheatre/co/nz/bt/WhatsOnFragment.java
+++ app/src/main/java/basementtheatre/co/nz/bt/WhatsOnFragment.java
@@ -1,11 +1,16 @@
 package basementtheatre.co.nz.bt;
 
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 
 /**
@@ -22,8 +27,24 @@ public class WhatsOnFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_whats_on_, container, false);
-    }
+      //  ImageButton button1 = (ImageButton) getView().findViewById(R.id.button1);
+     //  button1.setOnClickListener(new View.OnClickListener() {
 
+        //    @Override
+        //    public void onClick(View v) {
+                //   FragmentManager fm = getFragmentManager();
+                //   ft.commit();
+                //  FragmentTransaction ft = fm.beginTransaction();
+
+                // }
+
+                //   });
+                //   }
+        //    }
+     //   });
+    }
 }
+
+
+
