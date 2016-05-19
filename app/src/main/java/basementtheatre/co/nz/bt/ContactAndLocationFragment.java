2a66967309fd219715846a5bf9047fc23d031019
kilarbourbon <kilarbourbon@gmail.com>
2016-05-19 17:07:39 +1200

    Google map , currently working on adding mark, this is just a working version of the map fragment. Kai

--- app/src/main/java/basementtheatre/co/nz/bt/ContactAndLocationFragment.java
+++ app/src/main/java/basementtheatre/co/nz/bt/ContactAndLocationFragment.java
@@ -8,51 +8,68 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RelativeLayout;
-
 import com.google.android.gms.maps.*;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 
-public class ContactAndLocationFragment extends Fragment {
+public class ContactAndLocationFragment extends Fragment implements OnMapReadyCallback{
 
     private static View view;
     /**
      * Note that this may be null if the Google Play services APK is not
      * available.
      */
-
     private static GoogleMap mMap;
 
     private static Double latitude, longitude;
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+
         if (container == null) {
             return null;
         }
+
         view = (RelativeLayout) inflater.inflate(R.layout.fragment_contact_and_location, container, false);
         // Passing harcoded values for latitude & longitude. Please change as per your need. This is just used to drop a Marker on the Map
-        latitude = 26.78;
-        longitude = 72.56;
+        latitude = -36.853654439472;
+        longitude = 174.76280283467;
 
         setUpMapIfNeeded(); // For setting up the MapFragment
+
 
         return view;
     }
 
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+            mMap = map;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        setUpMapIfNeeded();
+    }
     /***** Sets up the map if it is possible to do so *****/
     public static void setUpMapIfNeeded() {
         // Do a null check to confirm that we have not already instantiated the map.
 
         if (mMap == null) {
             // Try to obtain the map from the SupportMapFragment.
-         ////   mMap = ((SupportMapFragment) HomeClass.fragmentManager.findFragmentById(R.id.ContactAndLocationFragment)).getMap();
+             //mMap = ((SupportMapFragment) HomeClass.fragmentManager.findFragmentById(R.id.ContactAndLocationFragment)).getMap(); // getMap is deprecated
+            //mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.ContactAndLocationFragment)).getMap();
             // Check if we were successful in obtaining the map.
             if (mMap != null)
                 setUpMap();
         }
     }
+
+
 
     /**
      * This is where we can add markers or lines, add listeners or move the
@@ -64,7 +81,7 @@ public class ContactAndLocationFragment extends Fragment {
     private static void setUpMap() {
 
         // For dropping a marker at a point on the Map
-        mMap.addMarker(new MarkerOptions().position(new LatLng(latitude, longitude)).title("My Home").snippet("Home Address"));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(latitude, longitude)).title("Basement Theatre").snippet("Home Address"));
         // For zooming automatically to the Dropped PIN Location
         mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(latitude,
                 longitude), 12.0f));
@@ -73,12 +90,17 @@ public class ContactAndLocationFragment extends Fragment {
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         // TODO Auto-generated method stub
+       // mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+      //  mMap.addMarker(new MarkerOptions().position(new LatLng(latitude, longitude)).title("Basement Theatre").snippet("Basement Theatre"));
+
+
+
         if (mMap != null)
             setUpMap();
 
         if (mMap == null) {
             // Try to obtain the map from the SupportMapFragment.
-        /////    mMap = ((SupportMapFragment) HomeClass.fragmentManager.findFragmentById(R.id.ContactAndLocationFragment)).getMap(); // getMap is deprecated
+           //mMap = ((SupportMapFragment) HomeClass.fragmentManager.findFragmentById(R.id.ContactAndLocationFragment)).getMap(); // getMap is deprecated
             // Check if we were successful in obtaining the map.
             if (mMap != null)
                 setUpMap();
