80f48b00d3b270801f35b7077629f091adcd7234
Dawid <gutladavid@gmail.com>
2016-06-07 10:34:12 +1200

    Push notifications

--- app/src/main/java/basementtheatre/co/nz/bt/MainActivity.java
+++ app/src/main/java/basementtheatre/co/nz/bt/MainActivity.java
@@ -10,12 +10,14 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import com.onesignal.OneSignal;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        OneSignal.startInit(this).init();
         setContentView(R.layout.activity_main);
         Button btn = (Button) findViewById(R.id.button);
         btn.setOnClickListener(new View.OnClickListener() {
