cd008354f05bddbb18e8f0104ef7f8ec08b5b644
aaronmorpeth <aaronm888@hotmail.com>
2016-06-15 17:39:00 +1200

    Updated photos

--- app/src/main/java/basementtheatre/co/nz/bt/WhatsOnFragment.java
+++ app/src/main/java/basementtheatre/co/nz/bt/WhatsOnFragment.java
@@ -23,7 +23,7 @@ public class WhatsOnFragment extends Fragment {
         // Required empty public constructor
     }
 
-//
+////
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
